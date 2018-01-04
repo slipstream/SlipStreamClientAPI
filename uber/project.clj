abf9ea8fb565462ce937c3b5d98ef04065caa730
khaled basbous <khaled@sixsq.com>
2018-01-04 20:39:26 +0100

    minor

--- uber/project.clj
+++ uber/project.clj
@@ -15,7 +15,7 @@
             [lein-localrepo "0.5.4"]]
 
   :parent-project {:coords  [com.sixsq.slipstream/parent "3.42-SNAPSHOT"]
-                   :inherit [:min-lein-version :managed-dependencies :repositories :deploy-repositories :plugins]}
+                   :inherit [:min-lein-version :managed-dependencies :repositories :deploy-repositories]}
 
   :pom-location "target/"
 
